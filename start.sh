eead3556b003e8ccdfb716a6aa1849de31f2cf5a
kangtiancheng <kangtiancheng001@163.com>
2019-07-08 16:17:50 +0800

    切换域名

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-java -jar target/lybbs/lybbs-latest.jar --spring.profiles.active=prod > log.file 2>&1 &
+# 拉取最新的源码
+git pull
+
+# 执行打包
+mvn clean package -Pprod
+
+java -jar target/lybbs.jar --spring.profiles.active=prod  &
+
+echo "Lybbs部署完毕，Enjoy！"
